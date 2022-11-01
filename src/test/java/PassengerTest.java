addcf02078379d6f879883d5aaa25f5f15087f94
Corey4711 <coreyegan673@gmail.com>
2022-11-01 16:40:11 +0000

    CICD-0003: Added All passenger get/set + tests

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -11,7 +11,7 @@ class PassengerTest {
 
     @BeforeEach
     void setUp() {
-        passenger = new Passenger(null,null,0,0,0);
+        passenger = new Passenger(null,null,null,null,0);
     }
 
     @Test
@@ -33,4 +33,45 @@ class PassengerTest {
     void titleFail(){
         assertThrows(IllegalArgumentException.class, () ->{passenger.setTitle(null);});
     }
+
+    @Test
+    void nameSuccess(){
+        passenger.setName("John");
+        assertEquals("John",passenger.getName());
+    }
+    @Test
+    void nameFail(){
+        assertThrows(IllegalArgumentException.class, () ->{passenger.setName("PJ");});
+    }
+
+    @Test
+    void idSuccess(){
+        passenger.setId("87621687621");
+        assertEquals("87621687621",passenger.getID());
+    }
+    @Test
+    void idFail(){
+        assertThrows(IllegalArgumentException.class, () ->{passenger.setId("123");});
+    }
+
+    @Test
+    void phoneSuccess(){
+        passenger.setPhone("08668463165");
+        assertEquals("08668463165",passenger.getPhone());
+    }
+    @Test
+    void phoneFail(){
+        assertThrows(IllegalArgumentException.class, () ->{passenger.setPhone("999");});
+    }
+
+    @Test
+    void ageSuccess(){
+        passenger.setAge(21);
+        assertEquals(21,passenger.getAge());
+    }
+    @Test
+    void ageFail(){
+        assertThrows(IllegalArgumentException.class, () ->{passenger.setAge(12);});
+    }
+
 }
